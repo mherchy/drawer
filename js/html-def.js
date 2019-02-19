26f39bc5d681caf84e9a3bdea9bb1ee6b878f660
Matthieu HERCHY <matthieu.herchy@enedis-grdf.fr>
2019-02-19 09:51:34 +0100

    Initial commit

---
+++ js/html-def.js
@@ -0,0 +1,244 @@
+class DrawerElement extends HTMLElement {
+
+    constructor()
+    {
+        super();
+
+        this.initialClass = [];
+        this.initialAttributes = [];
+        this.initialChildren = [];
+        this.initialCallbacks = [];
+    }
+
+    connectedCallback()
+    {
+        let elm = this;
+
+        // Class initiales
+        this.initialClass.forEach(function (c) {
+            elm.classList.add(c);
+        });
+
+        // Autres attributs
+        this.initialAttributes.forEach(function (obj) {
+            let key = Object.keys(obj)[0];
+            let val = Object.values(obj)[0];
+            console.log(`Attributs : ${key}:${val}`);
+            elm.setAttribute(key, val);
+        });
+
+        // Children
+        this.initialChildren.forEach(function (nd) {
+            elm.append(nd);
+        });
+
+        // Fonctions
+        this.initialCallbacks.forEach(function (f) {
+            f(elm);
+        });
+
+    }
+
+    addInitialClass(classe)
+    {
+        if(Array.isArray(classe))
+        {
+            let elm = this;
+            classe.forEach(function (c) {
+                elm.initialClass.push(c);
+            });
+        }
+        else if(typeof classe === "string")
+        {
+            this.initialClass.push(classe);
+        }
+    }
+
+
+    addInitialAttribute(attr, val)
+    {
+        this.initialAttributes.push(JSON.parse(`{"${attr}":"${val}"}`));
+    }
+
+    addInitialCallback(c)
+    {
+        this.initialCallbacks.push(c);
+    }
+
+    addInitialChildren(node)
+    {
+        this.initialChildren.push(node);
+    }
+
+
+}
+
+
+/**
+ * drw-area
+ *
+ */
+window.customElements.define("drw-area", class extends DrawerElement {
+    constructor()
+    {
+        super();
+        this.addInitialClass("row");
+    }
+});
+
+
+/**
+ * drw-area-elm
+ *
+ */
+window.customElements.define("drw-area-elm", class extends DrawerElement {
+    constructor()
+    {
+        super();
+        this.addInitialClass("col-6");
+    }
+});
+
+
+/**
+ * drw-area-children
+ *
+ *
+ */
+window.customElements.define("drw-area-children", class extends DrawerElement {
+    constructor()
+    {
+        super();
+        this.addInitialClass("col-6");
+    }
+});
+
+
+/**
+ * drw-elm
+ *
+ *
+ */
+window.customElements.define("drw-elm", class  extends DrawerElement {
+    constructor()
+    {
+        super();
+        this.addInitialClass('card');
+        this.addInitialAttribute('role', 'tablist');
+    }
+});
+
+
+/**
+ * drw-grp-name
+ *
+ *
+ */
+window.customElements.define("drw-grp-name", class  extends DrawerElement {
+    constructor()
+    {
+        super();
+
+        this.grp_member_attached = null;
+
+        this.addInitialClass('card-header');
+        this.addInitialAttribute('data-toggle', 'collapse');
+    }
+});
+
+
+/**
+ * drw-grp-members
+ *
+ *
+ */
+window.customElements.define("drw-grp-members", class extends DrawerElement {
+    constructor()
+    {
+        super();
+
+        this.addInitialClass('list-group');
+        this.addInitialClass('list-group-flush');
+        this.addInitialClass('collapse');
+
+    }
+});
+
+
+/**
+ * drw-grp-member
+ *
+ *
+ */
+window.customElements.define("drw-grp-member", class extends DrawerElement {
+    constructor()
+    {
+        super();
+
+        this.addInitialClass('list-group-item');
+        this.addInitialClass('list-group-item-action');
+
+        this.addInitialAttribute('role', 'tab');
+
+    }
+});
+
+
+/**
+ * drw-member-add
+ *
+ *
+ */
+window.customElements.define("drw-member-add", class extends DrawerElement {
+    constructor()
+    {
+        super();
+
+        let spanplus = document.createElement('span');
+        spanplus.className = 'fas fa-plus';
+        this.addInitialChildren(spanplus);
+
+
+        this.addInitialClass('list-group-item');
+        this.addInitialClass('list-group-item-action');
+    }
+});
+
+
+/**
+ * drw-children-container
+ *
+ *
+ */
+window.customElements.define("drw-children-container", class extends DrawerElement {
+    constructor()
+    {
+        super();
+
+        this.addInitialClass('tab-content');
+
+    }
+});
+
+
+/**
+ * drw-child-pane
+ *
+ *
+ */
+window.customElements.define("drw-child-pane", class extends DrawerElement {
+    constructor()
+    {
+        super();
+
+        this.addInitialClass('tab-pane');
+        this.addInitialClass('fade');
+
+        this.addInitialAttribute('role', 'tabpanel');
+
+    }
+});
+
+
+
+
